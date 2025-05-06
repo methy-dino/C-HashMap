e2ff9c69c0097094b0828c9428b4bfe346c231e9
methy-dino <gutotlima@gmail.com>
2025-05-06 18:29:23 -0300

    updated to be fully ansi c compliant.

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #include "tests.h"
 /* file with an example of how to use the library.*/
 size_t strHash2(void* key){
-    unsigned int code = 0;
-    unsigned int i = 0;
+    unsigned int code;
+    unsigned int i;
+		code = 0;
+		i = 0;
     while (((char*)key)[i] != '\0'){
         code += ((char*)key)[i];
         i++;
@@ -23,8 +25,16 @@ void freeStr(Entry* entry){
 	free(entry->key);
 	free(entry->value);
 }
-int main(int argc, char *argv[]){
-    HashMap* a = createMap(2, &strHash, &isSame, &freeStr);
+int main(){
+    HashMap* a;
+    int i;
+    char* str1;
+    char* str2;
+    char* str3;
+    char* str4;
+    char* str5;
+    char ss[2] = {'a','\0'};
+		a = createMap(2, &strHash, &isSame, &freeStr);
 		printf("aa\n");
     /* THIS IS A WRONG WAY TO ADD PAIRS, SINCE YOU SHOULD ALLOC THEN ON THE HEAP*/
     addPair(a,(void*)"a",(void*)"b");
@@ -32,28 +42,26 @@ int main(int argc, char *argv[]){
     addPair(a,(void*)"d",(void*)"eeeee");
     printf("%s\n",  (char*)getValue(a,"d"));
     /*test for no collisions*/
-    int i = 0;
 		for (i = 0; i <  200; i++){
         HashMap* map = createMap(6, &strHash2, &isSame, &freeStr);
 
-        char* str1 =(char*) malloc(2);
+        str1 =(char*) malloc(2);
         str1[0] = 'a';
         str1[1] = '\0';
-        char* str2 =(char*) malloc(2);
+        str2 =(char*) malloc(2);
         str2[0] = '1';
         str2[1] = '\0';
-        char* str3 =(char*) malloc(2);
+        str3 =(char*) malloc(2);
         str3[0] = 'b';
         str2[1] = '\0';
-        char* str4 =(char*) malloc(2);
+        str4 =(char*) malloc(2);
         str4[0] = '2';
         str4[1] = '\0';
         addPair(map, str1, str2);
         addPair(map, str3, str4);
-        char* str5 =(char*) malloc(2);
+        str5 =(char*) malloc(2);
         str5[0] = 'a';
         str5[1] = '\0';
-        char ss[] = "a";
         printf("%s",  (char*)getValue(map, ss));
     }
     unitTests();
--- tests.c
+++ tests.c
@@ -5,38 +5,45 @@
 #include <string.h>
 void printEnt(Entry* ent){	
     printf("details of entry at %p\n", (void*) ent);
-	printf("it's key is: %s\n", ent->key);
-	printf("it's value is: %s\n", ent->value);
+	printf("it's key is: %s\n", (char*) ent->key);
+	printf("it's value is: %s\n", (char*) ent->value);
 }
 int unitTests(){
-	int i = 0;
+	int i;
+	char* str1;
+	char* str2;
+	char* str3;
+	char* str4;
+	char* str5;
+	char* str6;
     for (i = 0; i < 2000; i++){
-        HashMap* map = createMap(8, &strHash, &strcmpWrap, &defaultFree);
-        char* str1 =(char*) malloc(2);
+        HashMap* map = createMap(1, &strHash, &strcmpWrap, &defaultFree);
+        str1 =(char*) malloc(2);
         str1[0] = 'a';
         str1[1] = '\0';
-        char* str2 =(char*) malloc(2);
+        str2 =(char*) malloc(2);
         str2[0] = '1';
         str2[1] = '\0';
-        char* str3 =(char*) malloc(2);
+        str3 =(char*) malloc(2);
         str3[0] = 'b';
         str3[1] = '\0';
-        char* str4 =(char*) malloc(2);
+        str4 =(char*) malloc(2);
         str4[0] = '2';
         str4[1] = '\0';
         addPair(map, str1, str2);
         addPair(map, str3, str4);
-        char* str5 =(char*) malloc(2);
+        str5 =(char*) malloc(2);
         str5[0] = 'e';
         str5[1] = '\0';
-				char* str6 =(char*) malloc(2);
+				str6 =(char*) malloc(2);
         str6[0] = 'e';
         str6[1] = '\0';
 				addPair(map, str5, str6);
         debugPrintMap(map, &printEnt, 2);
 				removeKey(map, str5);
-				printf("%p %s teste\n", getValue(map, str3), getValue(map, str3));
+				printf("%p %s teste\n", getValue(map, str3), (char*)getValue(map, str3));
         discardMap(map);
     }
     printf("memleak pass\n");
+		return 1;
 }
